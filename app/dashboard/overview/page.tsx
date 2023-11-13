adb985f1a4205a4087dfa410389072ce80c95abb
dave-rushabh <rushabh.dave24@gmail.com>
2023-11-13 17:50:55 +0530

    learnt streaming with dynamic rendering in Next 14

--- app/dashboard/overview/page.tsx
+++ app/dashboard/overview/page.tsx
@@ -33,8 +33,8 @@ const Page = async () => {
         />
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        <RevenueChart revenue={revenue} />
-        <LatestInvoices latestInvoices={latestInvoices} />
+        {/* <RevenueChart revenue={revenue} /> */}
+        {/* <LatestInvoices latestInvoices={latestInvoices} /> */}
       </div>
     </main>
   );
